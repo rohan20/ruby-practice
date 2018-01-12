0991872c3514a752d46f2ab4b4008c82a2619923
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-01-12 18:11:42 +0530

    looping a list

--- first-try.rb
+++ first-try.rb
@@ -1,3 +1,8 @@
+footballers = ["Ronaldo", "Messi", "Rooney", "Ibrahimovic", "Pogba"]
+for j in 0...footballers.length
+	print j, " : ", footballers[j], "\n"
+end
+
 for i in 1...10
 	print i, " Ruby\n"
 end
