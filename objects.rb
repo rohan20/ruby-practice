47d2c55494ad04bdc8a1b85c86ecf8ccea41a124
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-01-14 18:17:11 +0530

    added a method to the Person class

--- objects.rb
+++ objects.rb
@@ -1,5 +1,10 @@
 class Person
 	attr_accessor :name, :age, :outfit
+
+	def say_hello
+		@name + " says Hello!\n"
+	end
+
 end
 
 rohan = Person.new
@@ -7,3 +12,5 @@ rajat = Person.new
 
 rohan.name = "Rohan"
 rajat.name = "Rajat"
+
+print rohan.say_hello
