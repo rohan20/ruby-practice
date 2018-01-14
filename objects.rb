9a0bbbc57ca11f92d972052b22b58b1d39fde5c2
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-01-14 18:14:21 +0530

    created anotherobject

--- objects.rb
+++ objects.rb
@@ -2,7 +2,8 @@ class Person
 	attr_accessor :name, :age, :outfit
 end
 
-person1 = Person.new
+rohan = Person.new
+rajat = Person.new
 
-person1.name = "Rohan"
-print person1.name, "\n"
+rohan.name = "Rohan"
+rajat.name = "Rajat"
