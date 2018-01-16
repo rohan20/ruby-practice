c3a7cfed465c5fbc860153c58c7de599fb3eab13
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-01-16 12:29:48 +0530

    adding elements to an array

---
+++ arrays.rb
@@ -0,0 +1,28 @@
+footballers = ["Messi", "Ronaldo", "Pogba", "Ibra"]
+
+print footballers
+print "\n"
+
+#adding an element
+
+	#to a fixed position
+
+	footballers[4] = "Rooney"
+
+	print footballers
+	print "\n"
+
+	#to the end
+
+	footballers.push("De Gea")
+
+	print footballers
+	print "\n"
+
+	#alternative syntax for pushing an element to the end
+
+	footballers << "Smalling"
+
+	print footballers
+	print "\n"
+
