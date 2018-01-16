f8bd3ea36968b45aee040db56c65bdb4aad25722
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-01-16 12:09:44 +0530

    trying basic math operations

--- numbers.rb
+++ numbers.rb
@@ -2,3 +2,18 @@ print 3.2.class
 print "\n"
 print 4.class
 print "\n"
+
+print 12+5
+print "\n"
+
+print 12-5
+print "\n"
+
+print 12/5
+print "\n"
+
+print 12*5
+print "\n"
+
+print 12.0/5
+print "\n"
