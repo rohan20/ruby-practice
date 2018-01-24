f5defecbb35a96fbf610b459e8018285ce575582
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-01-24 20:19:05 +0530

    using operators with different data types

--- string-methods.rb
+++ string-methods.rb
@@ -8,10 +8,17 @@ print "With bang: \n"
 uppercaseStatement.downcase!
 print uppercaseStatement
 print "\n"
-=end
 
 puts "xyz" * 3
 #puts puts a new line after printing
 
 print "xyz" * 4
 #print doesn't put a new line
+=end
+
+var1 = '5'
+var2 = 2
+
+puts var1 * var2 //55
+puts var2 * var1 //error
+puts var1 + var2 //error
