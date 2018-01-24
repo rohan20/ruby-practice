a65263d82ceddbbf34a7efdb2cfbfb323f910272
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-01-24 20:24:37 +0530

    conversion b/w floats, strings and integers

--- string-methods.rb
+++ string-methods.rb
@@ -18,7 +18,8 @@ print "xyz" * 4
 
 var1 = '5'
 var2 = 2
+var3 = 1.4
 
 puts var1 * var2 #55
-puts var1.to_i * var2 #10
-puts var1 + var2.to_s #52
+puts var1.to_f * var2 #10.0
+puts var1 + + var2.to_s + var3.to_s #521.4
