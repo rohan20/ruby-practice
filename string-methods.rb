cac61cf03b95b12cc74e38fe9b1a7ad97b31cda5
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-01-25 07:07:56 +0530

    upcase vs capitalize; swapcase

--- string-methods.rb
+++ string-methods.rb
@@ -22,10 +22,16 @@ var3 = 1.4
 puts var1 * var2 #55
 puts var1.to_f * var2 #10.0
 puts var1 + + var2.to_s + var3.to_s #521.4
-=end
 
 puts "Hi, What's your name?"
 name = gets
 puts "Hey " + name + ", you have a really nice name!"
 puts "Hey " + name.chomp + ", you have a really nice name!"
 #chomp removes any new line characters
+=end
+
+string = "hELl2"
+puts string
+puts string.swapcase
+puts string.capitalize #only first letter capitalized
+puts string.upcase #whole string capitalized
